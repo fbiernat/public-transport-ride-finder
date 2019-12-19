ad6dd3823c21e36a81f6dc482927e41909058b05
fbiernat <filipbiernat@gmail.com>
2019-12-20 00:16:23 +0100

    Code cleanup

--- ttss.py
+++ ttss.py
@@ -1,20 +1,20 @@
 import requests
 import time
 import sys
-from math import floor
+from math import floor, ceil
 import html
 
 # TODO: [-] dodanie przekazywania argumentów funkcji wyszukujących przystanki z linii poleceń
-#		[-] format czasu do odjazdu tramwaju (np <1min.)
-# 		[ ] rozwinięcie algorytmu wyszukiwania przejazdów (obsługa tras z przesiadkami)
+#       [-] format czasu do odjazdu tramwaju (np <1min.)
+#       [ ] rozwinięcie algorytmu wyszukiwania przejazdów (obsługa tras z przesiadkami)
 
-url = 'http://www.ttss.krakow.pl/internetservice/services/'
+API_URL = 'http://www.ttss.krakow.pl/internetservice/services/'
 
 
 # curl http://www.ttss.krakow.pl/internetservice/services/lookup/autocomplete/json?query=123&language=en
 def getStop(stopName):
-    stopQueryUrl = url + 'lookup/autocomplete/json?query={}&language=en'
-    r = requests.get(stopQueryUrl.format(stopName))
+    stopQueryURL = API_URL + 'lookup/autocomplete/json?query={}&language=en'
+    r = requests.get(stopQueryURL.format(stopName))
     res = r.json()
 
     try:
@@ -50,7 +50,7 @@ def getStop(stopName):
 
 # curl http://www.ttss.krakow.pl/internetservice/services/passageInfo/stopPassages/stop?stop=125&mode=departure&language=en
 def getDepartureInfo(stopId):
-    departureRequestUrl = url + \
+    departureRequestUrl = API_URL + \
         'passageInfo/stopPassages/stop?stop={}&mode=departure&language=en'.format(
             stopId)
     res = requests.get(departureRequestUrl)
@@ -64,51 +64,25 @@ def getLine(length, character):
 
     return line
 
+def printMinutesToDeparture(seconds):
+  if seconds == '-':
+    return seconds
 
-def compareTime(time1, time2):
-    result = ''
-    deltaH = 0
-    if time2 == 'Brak':
-        return 'Brak'
+  if seconds >= 0:
+    minutes = floor(seconds / 60)
+    if minutes == 0:
+      return '<1min'
+  else:
+    minutes = ceil(seconds / 60)
 
-    t1 = time1.split(':')
-    t2 = time2.split(':')
-
-    t1[0] = int(t1[0])
-    t1[1] = int(t1[1])
-    t2[0] = int(t2[0])
-    t2[1] = int(t2[1])
-
-    # if t1[0] >= t2[0] and t1[1] >= t2[1]:
-    #     return 'Odjechał'
-
-    # if t1[0] < t2[0]:
-    #     deltaH = t2[0] - t1[0]
-    #     t2[1] += 60 * deltaH
-    # result += str(t2[0] - t1[0]) + 'h'
-
-    if t1[1] < t2[1]:
-        delta = t2[1] - t1[1]
-        if delta == 0:
-            return '<1min'
-        else:
-            if deltaH > 0:
-                delta += deltaH * 60
-            if delta >= 60:
-                hours = floor(delta / 60)
-                if hours > 1:
-                    result += str(hours) + 'h'
-                delta -= hours * 60
-            result += str(delta) + 'min'
-
-    return result
-
+  return str(minutes) + 'min'
 
 def main():
     if (len(sys.argv) != 1 and len(sys.argv) != 3):
         print('Sposob użycia python3 ttss.py nazwa-przystanku-poczatkowego nazwa-przystanku-koncowego')
         return
 
+    # print logo
     with open('logo.txt') as l:
         lines = l.readlines()
         for line in lines:
@@ -126,21 +100,28 @@ def main():
             print('START: ' + start['name'])
             while True:
                 stop = getStop(input('Podaj nazwe przystanku koncowego '))
+                if stop['id'] == start['id']:
+                    print('Wybrano ten sam przystanek')
+                    continue
                 if stop != 0:
                     break
             print('STOP: ' + stop['name'])
         except KeyboardInterrupt:
-            print()
+
             return
     else:
         if len(sys.argv) == 3:
             start = getStop(sys.argv[1])
             stop = getStop(sys.argv[2])
+            if start['id'] == stop['id']:
+                print('Wybrano ten sam przystanek')
+                return
 
     if start == None or stop == None:
         print('Brak danych, sproboj jeszcze raz')
         return
 
+    print()
     trasa = '{} - {}'.format(start['name'], stop['name'])
     print(trasa)
 
@@ -155,34 +136,30 @@ def main():
     for line in getDepartureInfo(start['id'])['actual']:
         if line['direction'] in directions:
             departures.append(line)
-
+    
     lineLength = 46
-
     print(getLine(lineLength, '-'))
-
-    global time
-    localtime = time.localtime(time.time())
-    hourNow = localtime.tm_hour
-    minutesNow = localtime.tm_min
-    timeNow = str(hourNow) + ':' + str(minutesNow)
 
     # wyswietl odjazdy z przystanku poczatkowego
     if len(departures) == 0:
         print('Brak przejazdów')
+        print(getLine(lineLength, '-'))
+
     else:
-        print('{} {:^20} {:5} {}'.format(
+        print('{} {:20} {:5} {}'.format(
             'Odjazd wg', 'Kierunek', 'Linia', 'Odjazd za'))
         print('rozkladu')
 
         print(getLine(lineLength, '-'))
 
-        for dep in departures:
+        for daparture in departures:
             # print(dep)
-            time = dep.get('plannedTime', 'Brak')
+            time = daparture.get('plannedTime', 'Brak')
+            relTime = daparture.get('actualRelativeTime', '-')
             print('{:9} {:20} {:5} {}'.format(
-                time, dep['direction'], dep['patternText'], compareTime(timeNow, time)))
-
-        print(getLine(lineLength, '-'))
-
+                time, daparture['direction'], daparture['patternText'], 
+                printMinutesToDeparture(relTime)))
+                
+            print(getLine(lineLength, '-'))
 
 main()
